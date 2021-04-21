0cc54439636aaeb39c3be61115d8b39a11ad1ef7
Harry Peach (Personal Signing Key) <harrypeach1999@gmail.com>
2021-04-21 21:41:44 +0100

    Add wall particle

--- sand_game/__main__.py
+++ sand_game/__main__.py
@@ -1,4 +1,5 @@
 from sand_game.particles.SandParticle import SandParticle
+from sand_game.particles.WallParticle import WallParticle
 from sand_game.canvas import CanvasController
 from sand_game.draw_utils import draw_cursor
 import pyxel
@@ -6,10 +7,10 @@ import pyxel
 
 class SandGame:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(160, 120, fps=60)
 
-        self.canvas_width = 64
-        self.canvas_height = 64
+        self.canvas_width = 100
+        self.canvas_height = 100
         self.canvas_start_loc = (10, 10)
         self.canvas_controller = CanvasController(
             self.canvas_width, self.canvas_height)
@@ -25,6 +26,11 @@ class SandGame:
             self.canvas_controller.set(
                 pyxel.mouse_x - self.canvas_start_loc[0],
                 pyxel.mouse_y - self.canvas_start_loc[1], SandParticle())
+
+        if pyxel.btn(pyxel.MOUSE_RIGHT_BUTTON):
+            self.canvas_controller.set(
+                pyxel.mouse_x - self.canvas_start_loc[0],
+                pyxel.mouse_y - self.canvas_start_loc[1], WallParticle())
 
         for x in range(self.canvas_width):
             for y in range(self.canvas_height):
---
+++ sand_game/particles/WallParticle.py
@@ -0,0 +1,19 @@
+from sand_game.particles.Particle import Particle
+from sand_game.canvas import CanvasController
+
+
+class WallParticle(Particle):
+    def __init__(self):
+        self.updated = False
+        self.color = 13
+
+    def update(self, x: int, y: int, canvas: CanvasController):
+        """Called when the particle must update its state
+
+        Args:
+            x (int): The current x location of the particle
+            y (int): The current y location of the particle
+            canvas (CanvasController): The current canvas' controller
+        """
+        self.updated = True
+        # self.fall(x, y, canvas)
